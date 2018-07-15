a64c0578406e5f091bd1f63de218cfc938935900
Anshul Rathi <anshulrathi03@gmail.com>
2018-07-16 05:23:49 +0530

    2nd Commit

--- Section01/BullsNCows/main.cpp
+++ Section01/BullsNCows/main.cpp
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
 int main()
 {
+	// Introduced the game
+
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls & Cows" << endl;
+	cout << "It's a fun word Game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram word I am thinking of right now ?" << endl;
+	cout << "\nEnter your answer here : " << endl;
+
+	// Getting Input from the player
+
+	string Guess = "";
+
+	cin >> Guess;
+	cout << "You entered the word : \n\n" << Guess << endl;
+
+	// Keep guessing the input from the player
+
+
 	return 0;
 }
